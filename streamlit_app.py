3307694fc2ebfb5a60c48d29ef0b2035dc348474
babarbaigmz <60806915+babarbaigmz@users.noreply.github.com>
2023-04-29 21:08:51 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,11 @@ def get_fruit_load_list():
     my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
     return my_cur.fetchall()
 
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute(f"insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('{new_fruit}')")
+    return f"Thanks for adding {new_fruit}"
+
 ################################ Fruity Section ############################################################
 streamlit.header("Fruity Vice Advice!!")
 try:
@@ -42,8 +47,17 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
+################################# Add new fruit#############################################################################
+add_my_fruit=streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function=insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
+  streamlit.text(back_from_frunction)
+  
 ###################################################################################################################
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows=get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
