afe74cedf65f28b3c38625121a608e5ce1a78f12
babarbaigmz <60806915+babarbaigmz@users.noreply.github.com>
2023-04-29 20:54:48 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,11 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+def get_fruit_load_list():  
+  my_cur = my_cnx.cursor()
+  my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+  my_data_rows = my_cur.fetchall()
+
 ################################ Fruity Section ############################################################
 streamlit.header("Fruity Vice Advice!")
 try:
@@ -37,19 +42,10 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
-##########################################################################################
-#fruit_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
-#streamlit.write('Thankyou for adding ', fruit_add)
-#fruityvice_response_add = requests.get("https://fruityvice.com/api/fruit/"+fruit_add)
-#fruityvice_normalized_add = pandas.json_normalize(fruityvice_response_add.json())
-#streamlit.dataframe(fruityvice_normalized_add)
-########################################################################################
+###################################################################################################################
 streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST") # values ('from streamlit123')")
-my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
 
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
